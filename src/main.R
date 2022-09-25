fa3b7d6ce1d753dc4c0399715084cf44fd6598e0
Faizal Rohmat <immaddudin@gmail.com>
2022-09-25 21:32:36 +0700

    Minor typo and reorganization

--- src/main.R
+++ src/main.R
@@ -10,42 +10,46 @@ library(terra)
 library(tidyverse)
 library(MASS)
 
-# Load source scripts
-source("./Load_reproject_crop_MODIS.R")
-source("./Extract_reclassify_rasters.R")
-source("./Get_matrix_by_year.R")
-source("./Calculate_transition_matrix.R")
-source("./Calculate_average_transition_matrix.R")
-
-# Paths and inputs
+# Define paths and inputs
 modis_database_path = "../data/MCD12Q1/"
 dataset_selected = "LC_Type1"
 bounding_shapefile_path = "../data/example_bound/example_shapefile.shp"
 reclass_table_path = "../data/reclassification_table.csv"
 reclass_order_path = "../data/classification_factor_order.csv"
 
+# Load source scripts
+source("./Load_reproject_crop_MODIS.R")
+source("./Extract_reclassify_rasters.R")
+source("./Get_matrix_by_year.R")
+source("./Calculate_transition_matrices.R")
+source("./Calculate_average_transition_matrix.R")
+
 # Load reproject and crop MODIS
-modis_cropped_rasters = Load_reproject_crop_MODIS(modis_database_path,
-                                                  dataset_selected,
-                                                  bounding_shapefile_path)
+modis_cropped_rasters = 
+  Load_reproject_crop_MODIS(modis_database_path,
+                            dataset_selected,
+                            bounding_shapefile_path)
 
 # Extract and reclassify rasters into one reclassified table
-reclassified_table = Extract_reclassify_rasters(modis_cropped_rasters,
-                                                reclass_table_path,
-                                                reclass_order_path)
+reclassified_table = 
+  Extract_reclassify_rasters(modis_cropped_rasters,
+                             reclass_table_path,
+                             reclass_order_path)
 
 # Get list of years
 years = reclassified_table$Year %>% unique() %>% sort()
 
 # Get all LC matrices
-all_lc_matrices = lapply(years,
-                         function(year){
-                           Get_matrix_by_year(reclassified_table, year)})
+all_lc_matrices = 
+  lapply(years, 
+         function(year){
+           Get_matrix_by_year(reclassified_table, year)})
 
 # Calculate all transition matrices
 all_transition_matrices = 
   mapply(
-    function(a, b){Calculate_transition_matrix(reclassified_table, a, b)},
+    function(a, b){
+      Calculate_transition_matrix(reclassified_table, a, b)},
     years[1:(length(years)-1)], 
     years[2:length(years)], 
     SIMPLIFY = FALSE)
